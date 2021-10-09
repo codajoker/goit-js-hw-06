3763a9236f0690640337310fde83345fe4b73b03
codajoker <codajoker91@gmail.com>
2021-10-09 11:21:44 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,14 +8,25 @@ const refs = {
 refs.buttonCreate.addEventListener("click", onCreateBoxes);
 refs.buttonClear.addEventListener("click", onDestroyBoxes);
 function onCreateBoxes(event) {
-  while (refs.boxes.children.length < Number(refs.input.value)) {
-    const size = 30 + refs.boxes.children.length * 10;
+  console.log(Number(refs.input.value));
+  for (let i = 0; i < Number(refs.input.value); i += 1) {
     const divEl = document.createElement(`div`);
+    const size = 30 + i * 10;
+
     divEl.style.backgroundColor = getRandomHexColor();
     divEl.style.height = `${size}px`;
     divEl.style.width = `${size}px`;
     refs.boxes.append(divEl);
+
+    console.log(divEl);
   }
+  //   for (let i = 0; i < Number(refs.input.value); i = +1) {
+  //     const size = 30 + refs.boxes.children.length * 10;
+  //     const divEl = document.createElement(`div`);
+  //     divEl.style.backgroundColor = getRandomHexColor();
+  //
+  //     refs.boxes.append(divEl);
+  //   }
 }
 function onDestroyBoxes(event) {
   refs.boxes.innerHTML = ``;
@@ -23,3 +34,7 @@ function onDestroyBoxes(event) {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// let amount = Number(refs.input.value);
+// const createBoxes = (amount) => {
+//   console.log(amount);
+// };
